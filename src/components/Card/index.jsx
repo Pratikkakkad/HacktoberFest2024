6176d78effca9f3dab4d96400374fc1d9e8a0001
bhavya bhojani <bhavyabhojani55@gmail.com>
2024-10-12 22:49:06 +0530

    fix card design and made it responsive

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -10,9 +10,9 @@ export const Card = ({
   },
 }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    <div className="card flex flex-col justify-between h-full p-4 bg-white shadow-md rounded-lg">
       <img className="w-full h-48 object-cover" src={imageUrl} alt={name} />
-      <div className="px-6 py-4">
+      <div className="">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base">
           Monthly: {monthly_reward_points}
@@ -22,9 +22,9 @@ export const Card = ({
         </p>
         <p className="text-gray-700 text-base">Value: {reward_value}</p>
       </div>
-      <div className="px-6 py-4">
+      <div className="pt-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Learn More
+          View Details
         </button>
       </div>
     </div>
