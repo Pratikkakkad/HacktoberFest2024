580c15eaa71ef3bddaeaad6cf52c0982c7a47409
pratikk <pratikkakkad6@gmail.com>
2024-10-12 16:47:54 +0530

    added tailwind css for UI setup

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./home.scss";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return <div className="text-3xl font-bold underline">Home Page</div>;
 }
