e160f341bbdec2e3cf9b0dd14de3e2d19907008d
krupa <javiakrupas@gmail.com>
2024-10-12 18:52:01 +0530

    update header card and routes component and configuration

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import "./home.scss";
 
 export default function Home() {
-  return <div className="text-3xl font-bold underline">Home Page</div>;
+  return (
+    <div className="text-3xl font-bold h-full">
+      <div className="flex justify-center items-center h-full">
+        Home Page under construction
+      </div>
+    </div>
+  );
 }
