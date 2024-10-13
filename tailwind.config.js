95b2683a0bc8c41d697318b2654fb64e1f50ff3e
digi-grow <digigrow.technologies@gmail.com>
2024-10-13 14:57:08 +0530

    updated theme config and added classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-red": {
+          DEFAULT: "#e61d25", // Base color
+          50: "#fce5e6", // Very light
+          100: "#f9b7bb", // Light
+          200: "#f28a8f", // Lighter
+          300: "#eb5d64", // Slightly lighter
+          400: "#e63039", // Darker
+          500: "#e61d25", // Base color (DEFAULT)
+          600: "#c2191f", // Darker shade
+          700: "#9b1318", // Even darker
+          800: "#740e11", // Very dark
+          900: "#4e080a", // Almost black
+        },
+      },
+    },
   },
   plugins: [],
 };
